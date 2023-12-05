019a95c9bfb60e84ac00976b84d4f143fac0a74d
yuto <44987057+yutowac@users.noreply.github.com>
2023-12-05 15:38:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,311 @@
+const SCAN_OPTIONS = {
+    acceptAllAdvertisements: true,
+    keepRepeatedDevices: true
+};
+var BlueJelly = function(){
+  this.bluetoothDevice = null;
+  this.dataCharacteristic = null;
+  this.hashUUID ={};
+  this.hashUUID_lastConnected;
+  //callBack
+  this.onScan = function(deviceName){console.log("onScan");};
+  this.onConnectGATT = function(uuid){console.log("onConnectGATT");};
+  this.onRead = function(data, uuid){console.log("onRead");};
+  this.onWrite = function(uuid){console.log("onWrite");};
+  this.onStartNotify = function(uuid){console.log("onStartNotify");};
+  this.onStopNotify = function(uuid){console.log("onStopNotify");};
+  this.onDisconnect = function(){console.log("onDisconnect");};
+  this.onClear = function(){console.log("onClear");};
+  this.onReset = function(){console.log("onReset");};
+  this.onError = function(error){console.log("onError");};
+}
+
+
+//--------------------------------------------------
+//setUUID
+//--------------------------------------------------
+BlueJelly.prototype.setUUID = function(name, serviceUUID, characteristicUUID){
+  console.log('Execute : setUUID');
+  console.log(this.hashUUID);
+
+  this.hashUUID[name] = {'serviceUUID':serviceUUID, 'characteristicUUID':characteristicUUID};
+}
+
+
+//--------------------------------------------------
+//scan
+//--------------------------------------------------
+BlueJelly.prototype.scan = function(uuid){
+  return (this.bluetoothDevice ? Promise.resolve() : this.requestDevice(uuid,true))
+    .catch(error => {
+    console.log('Error : ' + error);
+    this.onError(error);
+  });
+}
+
+
+//--------------------------------------------------
+//requestDevice
+//--------------------------------------------------
+BlueJelly.prototype.requestDevice = function(uuid) {
+  console.log('Execute : requestDevice');
+  return navigator.bluetooth.requestDevice({
+    acceptAllDevices: true,
+    optionalServices: [this.hashUUID[uuid].serviceUUID]})
+    .then(device => {
+    this.bluetoothDevice = device;
+    this.bluetoothDevice.addEventListener('gattserverdisconnected', this.onDisconnect);
+    this.onScan(this.bluetoothDevice.name);
+  });
+}
+
+
+//--------------------------------------------------
+//connectGATT
+//--------------------------------------------------
+BlueJelly.prototype.connectGATT = function(uuid) {
+  if(!this.bluetoothDevice)
+  {
+    var error = "No Bluetooth Device";
+    console.log('Error : ' + error);
+    this.onError(error);
+    return;
+  }
+  if (this.bluetoothDevice.gatt.connected && this.dataCharacteristic) {
+    if(this.hashUUID_lastConnected == uuid)
+      return Promise.resolve();
+  }
+  this.hashUUID_lastConnected = uuid;
+
+  console.log('Execute : connect');
+  return this.bluetoothDevice.gatt.connect()
+    .then(server => {
+    console.log('Execute : getPrimaryService');
+    return server.getPrimaryService(this.hashUUID[uuid].serviceUUID);
+  })
+    .then(service => {
+    console.log('Execute : getCharacteristic');
+    return service.getCharacteristic(this.hashUUID[uuid].characteristicUUID);
+  })
+    .then(characteristic => {
+    this.dataCharacteristic = characteristic;
+    this.dataCharacteristic.addEventListener('characteristicvaluechanged',this.dataChanged(this, uuid));
+    this.onConnectGATT(uuid);
+  })
+    .catch(error => {
+    console.log('Error : ' + error);
+    this.onError(error);
+  });
+}
+
+
+//--------------------------------------------------
+//dataChanged
+//--------------------------------------------------
+BlueJelly.prototype.dataChanged = function(self, uuid) {
+  return function(event) {
+    self.onRead(event.target.value, uuid);
+  }
+}
+
+
+//--------------------------------------------------
+//read
+//--------------------------------------------------
+BlueJelly.prototype.read= function(uuid) {
+  return (this.scan(uuid))
+    .then( () => {
+    return this.connectGATT(uuid);
+  })
+    .then( () => {
+    console.log('Execute : readValue');
+    return this.dataCharacteristic.readValue();
+  })
+    .catch(error => {
+    console.log('Error : ' + error);
+    this.onError(error);
+  });
+}
+
+
+//--------------------------------------------------
+//write
+//--------------------------------------------------
+BlueJelly.prototype.write = function(uuid, array_value) {
+  return (this.scan(uuid))
+    .then( () => {
+    return this.connectGATT(uuid);
+  })
+    .then( () => {
+    console.log('Execute : writeValue');
+    data = Uint8Array.from(array_value);
+    return this.dataCharacteristic.writeValue(data);
+  })
+    .then( () => {
+    this.onWrite(uuid);
+  })
+    .catch(error => {
+    console.log('Error : ' + error);
+    this.onError(error);
+  });
+}
+
+
+//--------------------------------------------------
+//startNotify
+//--------------------------------------------------
+BlueJelly.prototype.startNotify = function(uuid) {
+  return (this.scan(uuid))
+    .then( () => {
+    return this.connectGATT(uuid);
+  })
+    .then( () => {
+    console.log('Execute : startNotifications');
+    this.dataCharacteristic.startNotifications()
+  })
+    .then( () => {
+    this.onStartNotify(uuid);
+  })
+    .catch(error => {
+    console.log('Error : ' + error);
+    this.onError(error);
+  });
+}
+
+
+//--------------------------------------------------
+//stopNotify
+//--------------------------------------------------
+BlueJelly.prototype.stopNotify = function(uuid){
+  return (this.scan(uuid))
+    .then( () => {
+    return this.connectGATT(uuid);
+  })
+    .then( () => {
+    console.log('Execute : stopNotifications');
+    this.dataCharacteristic.stopNotifications()
+  })
+    .then( () => {
+    this.onStopNotify(uuid);
+  })
+    .catch(error => {
+    console.log('Error : ' + error);
+    this.onError(error);
+  });
+}
+
+
+//--------------------------------------------------
+//disconnect
+//--------------------------------------------------
+BlueJelly.prototype.disconnect= function() {
+  if (!this.bluetoothDevice) {
+    var error = "No Bluetooth Device";
+    console.log('Error : ' + error);
+    this.onError(error);
+    return;
+  }
+
+  if (this.bluetoothDevice.gatt.connected) {
+    console.log('Execute : disconnect');
+    this.bluetoothDevice.gatt.disconnect();
+  } else {
+    var error = "Bluetooth Device is already disconnected";
+    console.log('Error : ' + error);
+    this.onError(error);
+    return;
+  }
+}
+
+
+//--------------------------------------------------
+//clear
+//--------------------------------------------------
+BlueJelly.prototype.clear= function() {
+  console.log('Excute : Clear Device and Characteristic');
+  this.bluetoothDevice = null;
+  this.dataCharacteristic = null;
+  this.onClear();
+}
+
+
+//--------------------------------------------------
+//reset(disconnect & clear)
+//--------------------------------------------------
+BlueJelly.prototype.reset= function() {
+  console.log('Excute : reset');
+  this.disconnect(); //disconnect() is not Promise Object
+  this.clear();
+  this.onReset();
+}
+
+
+//--------------------------------------------------
+//Global変数
+//--------------------------------------------------
+//BlueJellyのインスタンス生成
+var ble = new BlueJelly();
+
+
+//-------------------------------------------------
+//ボタンが押された時のイベント登録
+//--------------------------------------------------
+// document.getElementById('readData').addEventListener('click', function() {
+//   ble.read('BatteryLevel');
+// });
+// document.getElementById('reset').addEventListener('click', function() {
+//   ble.reset(); //reset is disconnect & clear
+// });
+
+
+//--------------------------------------------------
+//ロード時の処理
+//--------------------------------------------------
+window.onload = function() {
+  //初期の文字列表示
+  document.getElementById('device_name').innerHTML = "No Device";
+  document.getElementById('data_text').innerHTML = "No Data"
+
+  //UUIDの設定
+  ble.setUUID("BatteryLevel", "0000180f-0000-1000-8000-00805f9b34fb", "00002a19-0000-1000-8000-00805f9b34fb");
+}
+
+
+//--------------------------------------------------
+//Scan後の処理
+//--------------------------------------------------
+ble.onScan = function(deviceName) {
+  //HTMLに表示
+  document.getElementById('device_name').innerHTML = deviceName;
+}
+
+
+//--------------------------------------------------
+//Read後の処理：得られたデータの表示など行う
+//--------------------------------------------------
+ble.onRead = function(data, uuid) {
+  //フォーマットに従って値を取得
+  value = data.getUint8(0); //1Byteの場合のフォーマット
+
+  //HTMLに値を表示
+  document.getElementById('data_text').innerHTML = value;
+
+  //再びRead
+  ble.read('BatteryLevel');
+}
+
+
+//--------------------------------------------------
+//Reset後の処理
+//--------------------------------------------------
+ble.onReset = function() {
+  //HTMLに表示
+  document.getElementById('device_name').innerHTML = "No Device";
+}
+
+
+function clickBLE() {
+  ble.read('BatteryLevel');
+  alert("気になる？");
+  console.log("クリックされました");
+}
