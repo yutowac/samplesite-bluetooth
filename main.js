7249ce620696289d856d041d6718f84bdbc9f35c
yuto <44987057+yutowac@users.noreply.github.com>
2023-12-22 23:55:32 +0900

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,6 @@ var BlueJelly = function(){
   this.onError = function(error){console.log("onError");};
 }
 
-
 //--------------------------------------------------
 //setUUID
 //--------------------------------------------------
